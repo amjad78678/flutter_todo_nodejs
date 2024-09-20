fc87f518bdc1b1522d7338276b9a57f1211879ed
amjad78678 <81846358+amjad78678@users.noreply.github.com>
2024-09-20 08:55:15 +0530

    changing to next.js

--- src/routes/userRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getWork, postRegister } from "../controller/userController";
-import { auth } from "../middleware/auth";
-const router = express.Router();
-
-router.get("/", getWork);
-router.post("/register", auth, postRegister);
-
-export default router;
